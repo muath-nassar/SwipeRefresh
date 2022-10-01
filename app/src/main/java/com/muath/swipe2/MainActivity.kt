8a6adaf8207172fe9bc8bdc62924d68b407c3bb3
MuathNassar <m.nassar@unitone.co>
2022-10-01 12:45:40 +0300

    Initial commit

---
+++ app/src/main/java/com/muath/swipe2/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.muath.swipe2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.R
+import com.muath.swipe2.databinding.ActivityMainBinding
+import com.simform.refresh.SSPullToRefreshLayout
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding :ActivityMainBinding
+    var data = mutableListOf<String>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        for (i in 0 until 30){
+            data.add(i.toString())
+        }
+        binding.rv.adapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item,data)
+        setupPullToRefresh()
+    }
+    private fun setupPullToRefresh(){
+        binding.ssPullRefresh.setRepeatCount(SSPullToRefreshLayout.RepeatCount.INFINITE)
+        binding.ssPullRefresh.setRefreshView(WaveAnimation(this))
+        binding.ssPullRefresh.setRefreshStyle(SSPullToRefreshLayout.RefreshStyle.FLOAT)
+        binding.ssPullRefresh.setRefreshViewParams(ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,300))
+        binding.ssPullRefresh.setOnRefreshListener(object : SSPullToRefreshLayout.OnRefreshListener {
+            override fun onRefresh() {
+                // This is demo code to perform
+                GlobalScope.launch {
+                    delay(3000)
+                    binding.ssPullRefresh.setRefreshing(false) // This line stops layout refreshing
+                    MainScope().launch {
+                        Toast.makeText(this@MainActivity,"Refresh Complete",Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+        })
+    }
+}
---
+++ app/src/main/java/com/muath/swipe2/WaveAnimation.kt
@@ -0,0 +1,148 @@
+package com.muath.swipe2
+
+import android.animation.ValueAnimator
+import android.content.Context
+import android.graphics.*
+import androidx.core.graphics.minus
+import androidx.core.graphics.xor
+import com.simform.refresh.SSAnimationView
+
+class WaveAnimation(context: Context) : SSAnimationView(context) {
+    private var amplitude = 4f.toDp() // scale
+    private var speed = 0f
+    private val pathWave = Path()
+    private var pathBackground = Path()
+    private var textPath = Path()
+    private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private var paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private var backgroundPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private var animator: ValueAnimator? = null
+    private var cutBackgroundPath = Path()
+
+    override fun onDraw(c: Canvas) {
+        createBackground()
+        createRoundCutInBackground()
+        //pathBackground = pathBackground.minus(cutBackgroundPath)
+        pathBackground = pathBackground.minus(pathWave)
+        c.drawPath(pathBackground, backgroundPaint)
+        showTitle(c)
+
+    }
+
+    private fun createAnimator(): ValueAnimator {
+        return ValueAnimator.ofFloat(0f, Float.MAX_VALUE).apply {
+            repeatCount = ValueAnimator.INFINITE
+            addUpdateListener {
+                speed -= WAVE_SPEED
+                createPath()
+                invalidate()
+            }
+        }
+    }
+
+    private fun createBackground() {
+
+        pathBackground.reset()
+        pathBackground.fillType = Path.FillType.EVEN_ODD
+        backgroundPaint.color = Color.CYAN//parseColor("#203354")
+        pathBackground.moveTo(0f, 0f)
+        pathBackground.lineTo(width.toFloat(),0f)
+        pathBackground.lineTo(width.toFloat(),height.toFloat()-70)
+        pathBackground.lineTo(0f,height.toFloat()-70)
+        //pathBackground.lineTo(0f,0f)//in case of rectangle
+        pathBackground.arcTo(RectF(0f,height.toFloat()-140,width.toFloat(),height.toFloat()),0f,180f)
+        pathBackground.close()
+
+    }
+
+
+    private fun createRoundCutInBackground(){
+        if (animator==null || animator?.isRunning == false){
+            val rectF = RectF(0f,0f,width.toFloat(),0f)
+            cutBackgroundPath.reset()
+            cutBackgroundPath.moveTo(0f, height.toFloat())
+            cutBackgroundPath.lineTo(0f, 10f)
+            cutBackgroundPath.arcTo(rectF,0f,360f,false)
+            cutBackgroundPath.lineTo(width.toFloat(), 0f)
+            cutBackgroundPath.lineTo(0f, height.toFloat())
+
+            cutBackgroundPath.close()
+        }
+    }
+
+    private fun showTitle(c: Canvas) {
+        textPath.reset()
+        textPath = pathBackground
+        textPaint.textSize = 45f
+        textPaint.textAlign = Paint.Align.CENTER
+        textPaint.color = Color.WHITE
+        if (animator?.isRunning == true){
+            c.drawText("updating content", (width / 2).toFloat(), (height / 2).toFloat()-40, textPaint)
+
+        }else{
+            c.drawText("scroll down to update", (width / 2).toFloat(), (height / 2).toFloat(), textPaint)
+
+        }
+    }
+
+    private fun createPath() {
+        pathWave.reset()
+        paint.color = Color.WHITE//parseColor("#203354")
+        pathWave.moveTo(0f, height.toFloat())
+        pathWave.lineTo(0f, amplitude)
+        pathWave.lineTo(0f, amplitude - 10)
+        var i = 0
+        while (i < width + 10) {
+            val wx = i.toFloat()
+            val wy =
+                amplitude * 2 + amplitude * kotlin.math.sin((i + 10) * Math.PI / WAVE_AMOUNT_ON_SCREEN + speed) + 200f
+            pathWave.lineTo(wx, wy.toFloat())
+            i += 10
+        }
+        pathWave.lineTo(width.toFloat(), height.toFloat())
+        pathWave.close()
+        //pathBackground = pathBackground.minus(pathWave)
+
+
+    }
+
+    override fun onDetachedFromWindow() {
+        animator?.cancel()
+        super.onDetachedFromWindow()
+    }
+
+    companion object {
+        const val WAVE_SPEED = 0.25f
+        const val WAVE_AMOUNT_ON_SCREEN = 350
+    }
+
+    private fun Float.toDp() = this * context.resources.displayMetrics.density
+
+    override fun reset() {
+    }
+
+    override fun refreshing() {
+        animator = createAnimator().apply {
+            start()
+        }
+    }
+
+    override fun refreshComplete() {
+        animator?.cancel()
+        pathWave.reset()
+        textPath.reset()
+    }
+    override fun pullToRefresh() {
+        animator = createAnimator().apply {
+            start()
+        }
+    }
+
+    override fun releaseToRefresh() {
+
+    }
+
+    override fun pullProgress(pullDistance: Float, pullProgress: Float) {
+    }
+
+}
